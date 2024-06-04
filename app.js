96ad74c73feedf937c042f3e4be6d97612bcf6f3
Shivam Kumar <2022ugcs030@nitjsr.ac.in>
2024-06-04 14:29:55 +0530

    created nested elements in react

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const parent=React.createElement("div",{id:"parent"},
-React.createElement("div",{id:"child"},
+[React.createElement("div",{id:"child"},
 [React.createElement("h1",{id:"tt"},"I am H1"),React.createElement("h1",{id:"tu"},"I am H2")]
+),
+React.createElement("div",{id:"child2"},
+[React.createElement("h1",{id:"tt"},"I am H1"),React.createElement("h1",{id:"tu"},"I am H2")]
+)] //Since it's messy so jsx is used.
 )
-)
+
 console.log(parent);
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
