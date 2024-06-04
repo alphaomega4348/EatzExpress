58b2d35762774d2f34ad20244b4f9699295dbcbc
Shivam Kumar <2022ugcs030@nitjsr.ac.in>
2024-06-04 14:20:57 +0530

    feat: Update app.js to render a parent div with a child div and an h1 element

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-const heading=React.createElement("h1",{},"Hello World")
-const root=ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading)
+const parent=React.createElement("div",{id:"parent"},
+React.createElement("div",{id:"child"},
+React.createElement("h1",{id:"tt"},"I am H1")
+)
+)
+console.log(parent);
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
