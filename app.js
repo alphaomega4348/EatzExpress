f838e375e0241a55a65eff2955220395c80eb7d7
Shivam Kumar <2022ugcs030@nitjsr.ac.in>
2024-06-04 14:26:28 +0530

    feat: Add a second h1 element to the child div in app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const parent=React.createElement("div",{id:"parent"},
 React.createElement("div",{id:"child"},
-React.createElement("h1",{id:"tt"},"I am H1")
+[React.createElement("h1",{id:"tt"},"I am H1"),React.createElement("h1",{id:"tu"},"I am H2")]
 )
 )
 console.log(parent);
