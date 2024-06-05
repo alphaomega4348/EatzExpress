408299dd6541416ab755bbcf6b1bc24ff9a92010
Shivam Kumar <2022ugcs030@nitjsr.ac.in>
2024-06-05 11:23:07 +0530

    react basic functional components

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
 import React from "react"
 import ReactDOM from "react-dom" 
-for(let i=0;i<1000;i++){
-
+let num=2;
+const HeadingComponent =()=>{
+    return <h1>
+        {console.log(num)}
+        Yo Bro!!
+    </h1>
 }
-const parent=React.createElement("div",{id:"parent"},
-[React.createElement("div",{id:"child"},
-[React.createElement("h1",{id:"tt"},"I am H1"),React.createElement("h1",{id:"tu"},"I am H2")]
-),
-React.createElement("div",{id:"child2"},
-[React.createElement("h1",{id:"tt"},"I am H1"),React.createElement("h1",{id:"tu"},"I am H2")]
-)] //Since it's messy so jsx is used.
-)
 
-console.log(parent);
-
-const root=ReactDOM.createRoot(document.getElementById("root"));
-console.log(root);
-root.render(parent);
+const root= ReactDOM.createRoot(document.getElementById("root"))
+root.render(<HeadingComponent/>)
