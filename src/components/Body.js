05e37c07e6e3c27debed0dbfdde2c6ab3cbba1e7
Shivam Kumar <2022ugcs030@nitjsr.ac.in>
2024-06-07 08:19:28 +0530

    button for filtering

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,10 +2,16 @@ import RestaurantCard from "./RestaurantCard";
 import restaurantList from "../utils/mockData";
  const Body = () => {
     return (
+      <div>
+        <div className="filter">
+          <button className="filter-btn">View Top Resturants</button>
+        </div>
+      
       <div className="res-container">
         {restaurantList.map((restaurant) => {
           return <RestaurantCard key={restaurant.info.id} {...restaurant.info} />;
         })}
+      </div>
       </div>
     );
   };
