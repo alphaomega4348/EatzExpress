f3f1d353dea85fa59aef3b7d8036e156eba19bd0
Shivam Kumar <2022ugcs030@nitjsr.ac.in>
2024-06-08 14:48:53 +0530

    feat: Add search functionality to filter restaurants by name

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,13 +3,15 @@ import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
  const Body = () => {
   const [List,setList]=useState([]);
+  const dummy=List;
+  const [searchText,setSearchText]=useState([""]);
   const fetchData= async() => {
     const data= await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
       const json=await data.json();
+      console.log(json)   
       setList(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants)
-      console.log("JSON ",json);
   }
   useEffect(()=>{
     fetchData();
@@ -20,8 +22,24 @@ import Shimmer from "./Shimmer";
   }
   
     return (
-      <div>
+      <div className="body">
         <div className="filter">
+        <div className="search-container">
+            <input type="text" className="search-input" value={searchText} onChange={(e)=>{
+              setSearchText(e.target.value)
+            }}/>
+            <button 
+            onClick={()=>{
+          const filteredRestaurants=    List.filter((res)=>(
+                  res.info.name.toLowerCase().includes(searchText.toLowerCase() ) 
+          ))
+              setList(filteredRestaurants)
+              
+                
+            }} 
+            
+            className="search-button"> Search</button>
+        </div>
           <button onClick={()=>{
             filterList=List.filter((res)=>res.info.avgRating
             >4)
