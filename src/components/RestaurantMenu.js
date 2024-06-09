51645936c57c00cdf157743e1249af81c6343e1c
Shivam Kumar <2022ugcs030@nitjsr.ac.in>
2024-06-09 15:18:48 +0530

    refactor: Improve menu item rendering and add rating display

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -22,7 +22,7 @@ const RestaurantMenu = () => {
 
     const { name, cuisines, costForTwoMessage } = resInfo?.cards[2]?.card?.card?.info || {};
     const itemCards = resInfo?.cards[4]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card?.card?.itemCards || [];
-    
+    console.log(itemCards)
     return (
         <div className="restaurant-menu-content">
             <div className="menu-items-container">
@@ -31,12 +31,19 @@ const RestaurantMenu = () => {
                     <h4>{cuisines?.join(", ")} - {costForTwoMessage}</h4>
                 </div>
                 <div className="menu-items-list">
-                    {itemCards.map((item, index) => (
-                        <div key={index} className="menu-item">
+                    {itemCards.map((item, index) => {
+                        const rating = item.card.info.ratings?.aggregatedRating?.rating || 0;
+                        let ratingClass = '';
+                        if (rating >= 4) ratingClass = 'rating-green';
+                        else if (rating >= 3) ratingClass = 'rating-orange';
+                        else if (rating >= 2) ratingClass = 'rating-yellow';
+                        else if (rating > 0) ratingClass = 'rating-red';
+                       return( <div key={index} className="menu-item">
                             <div className="menu-item-details">
                                 <div className="item-title">{item.card.info.name}</div>
                                 <div className="item-cost">Rs. {(item.card.info.price || item.card.info.defaultPrice) / 100}</div>
-                                <div className="item-desc">{(item.card.info.ratings?.aggregatedRating?.rating || 0) + "⭐️"}</div>
+                                <div className="item-desc">{item.card.info.description||""}</div>
+                                {rating > 0 && <div className={`item-rating ${ratingClass}`}>{rating + "⭐️"}</div>}
                             </div>
                             <div className="menu-img-wrapper">
                                 <img
@@ -44,10 +51,10 @@ const RestaurantMenu = () => {
                                     src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/" + item.card.info.imageId}
                                     alt={item.card.info.name}
                                 />
-                                <button className="add-btn">Add to Cart</button>
+                                <button className="add-btn">Add</button>
                             </div>
-                        </div>
-                    ))}
+                        </div>)
+})}
                 </div>
             </div>
         </div>
