a5ae6b9e9d3fb0b3e9897170795b619d8cfd45c3
Shivam Kumar <2022ugcs030@nitjsr.ac.in>
2024-06-11 09:05:17 +0530

    refactor: Update Header component to display user name in a more prominent way

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,24 +3,29 @@ import { Link } from "react-router-dom";
 import Title from "./Title.js";
 import useOnlineStatus from "../utils/useOnlineStatus.js";
 import UserContext from "../utils/UserContext.js";
+
 const Header = () => {
   const onlineStatus = useOnlineStatus() ? "🟢" : "🔴";
-  const {loggedInUser}=useContext(UserContext)
-    return (
-      <div className="header">
-        <Title/>
-        <div className="nav-items">
-          <ul>
-            <li>{onlineStatus}</li>
-            <li> <Link to="/">Home</Link> </li>
-            <li> <Link to="/about">About</Link> </li>
-            <li> <Link to="/contact">Contact</Link> </li>
-            <li> <Link to="/cart">Cart</Link> </li>
-            <li className="font-bold">{loggedInUser}</li>
-          </ul> 
-        </div>
+  const {loggedInUser} = useContext(UserContext);
+
+  return (
+    <div className="header">
+      <Title/>
+      <div className="flex justify-center items-center mt-2.5">
+        <span className="font-bold text-green-600 text-xl mx-2">Hello,</span>
+        <span className="font-bold text-green-600  text-xl">{loggedInUser}</span>
       </div>
-    );
-  };
+      <div className=" nav-items">
+        <ul className="mt-6">
+          <li>{onlineStatus}</li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+          <li><Link to="/cart">Cart</Link></li>
+        </ul>
+      </div>
+    </div>
+  );
+}
 
 export default Header;
