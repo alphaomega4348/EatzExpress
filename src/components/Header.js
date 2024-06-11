a0ce8b537c884adb379ed14bb3a18613902164aa
Shivam Kumar <2022ugcs030@nitjsr.ac.in>
2024-06-12 05:14:16 +0530

    refactor: Update Header component to include shopping cart icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,26 +6,40 @@ import UserContext from "../utils/UserContext.js";
 import { FaCartShopping } from "react-icons/fa6";
 const Header = () => {
   const onlineStatus = useOnlineStatus() ? "🟢" : "🔴";
-  const {loggedInUser} = useContext(UserContext);
+  const { loggedInUser } = useContext(UserContext);
 
   return (
     <div className="header">
-      <Title/>
+      <Title />
       <div className="flex justify-center items-center mt-2.5">
         <span className="font-bold text-green-600 text-xl mx-2">Hello,</span>
-        <span className="font-bold text-green-600  text-xl">{loggedInUser}</span>
+        <span className="font-bold text-green-600  text-xl">
+          {loggedInUser}
+        </span>
       </div>
       <div className=" nav-items">
         <ul className="mt-6">
           <li>{onlineStatus}</li>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/contact">Contact</Link></li>
-          <li className="mt-1"><Link to="/cart"><FaCartShopping/></Link></li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <div className="nav-login-cart">
+          <Link to='/login'><button> Login</button></Link>
+            <Link to="/cart">
+             <FaCartShopping/>
+            </Link>
+            <div className="nav-cart-count">0</div>
+          </div>
         </ul>
       </div>
     </div>
   );
-}
+};
 
 export default Header;
