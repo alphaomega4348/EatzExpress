549c0fdb6a6bb6b9d843756c35d53ee0c2b53c65
Shivam Kumar <2022ugcs030@nitjsr.ac.in>
2024-06-12 08:45:37 +0530

    Updates

---
+++ Grocery.cb0884c9.js
@@ -0,0 +1,2 @@
+var e;(0,(e=("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire9d15).register)("dhpq2",function(n,r){Object.defineProperty(n.exports,"__esModule",{value:!0,configurable:!0}),Object.defineProperty(n.exports,"default",{get:function(){return i},set:void 0,enumerable:!0,configurable:!0});var o=e("cc6u1");e("fIwXP");var i=function(){return(0,o.jsx)("div",{children:"Grocery"})}});
+//# sourceMappingURL=Grocery.cb0884c9.js.map
